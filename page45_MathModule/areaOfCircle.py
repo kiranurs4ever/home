796be430f1020e383be187960201dc96edbd637b
kiranurs4ever <kiranurs4ever@gmail.com>
2019-12-29 20:17:59 +0530

    Page45_book

---
+++ page45_MathModule/areaOfCircle.py
@@ -0,0 +1,3 @@
+from math import pi
+r = 16
+print("Area of circle is ",pi*r**2)
---
+++ page45_MathModule/importMemberInModule.py
@@ -0,0 +1,6 @@
+# The following code shows you how to import only specific members in module
+# Also how to access that members
+
+from math import sqrt,pi
+print(sqrt(16))
+print(pi)
